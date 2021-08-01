103564d648413351e15b9813cd7aa9bf478acc3f
EDUARDO PINA <eduardopina@MacBook-Air-de-EDUARDO.local>
2021-07-31 21:03:35 -0300

    Cards' distibution randomized and css transition on hover added

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,25 +1,57 @@
 let lista = document.querySelector(".lista");
-let a = "";
+let a = [];
 let quantidade = Number(prompt('Escolha o número de cartas (números pares de 4 a 14)'));
-const cartinha = `<li onclick = "virarCarta()">
-    <img src = "Images/front.png" class = "front-parrot">
-</li>`;
 
-console.log(lista);
-console.log(a);
-
-
-const adicionar = function (){
-    if (quantidade >= 4 && quantidade <= 14 && !(quantidade%2)){
-    a += cartinha;
-    }    
+while (!(quantidade >= 4 && quantidade <= 14 && !(quantidade%2))){
+    quantidade = Number(prompt('Escolha o número de cartas (números pares de 4 a 14)'));
 }
 
-for(let i = 0; i < quantidade; i++){
-    adicionar();       
-}    
+const cartinhaAleatoria = function (){
+    let imagens = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif" ];
+    let indice = Math.ceil(Math.random() * 7) - 1;
+    console.log(indice);
 
-lista.innerHTML = a;
+    let cartinha = `
+    <li onclick = "virarCarta(this)">
+        <div class = "face front-face">
+            <img src = "Images/front.png" class = "front-parrot">
+        </div>
+        <div class = "face back-face">
+            <img src = "Images/${imagens[indice]}" class = "gif">
+        </div>
+    </li>`;
 
+    return cartinha;
+}
+
+const adicionarArray = function (){
+    let cartinha = cartinhaAleatoria();
+    a.push(cartinha, cartinha);
+}
+
+function ordemAleatoria() {
+    return (Math.round(Math.random())-0.5);
+}
+
+const AdicionarEmbaralhar = function (){
+    for(let i = 0; i < (quantidade/2); i++){
+        adicionarArray();       
+    }    
+
+    a.sort(ordemAleatoria);
+
+    for(let i = 0; i < quantidade; i++){
+        lista.innerHTML += a[i];
+    }
+}
+
+AdicionarEmbaralhar();
+
+const virarCarta = function(elemento) {
+    let backFace = elemento.querySelector(".face .back-face")
+    let face = elemento.querySelector(".face .front-face")
+    backFace.transform = "rotateY(0)"
+    face.transform = "rotateY(-180)"
+}
 
 
